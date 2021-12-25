6c37ca971b894ef2e09d339140a1760081395a96
Vikranth Kanumuru <20688709+highskillzz@users.noreply.github.com>
2021-12-26 00:49:53 +0530

    Basic initialization of nodejs

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const validUrl = require("valid-url");
+const shortid = require("shortid");
+
+// instatiate the express app
+const app = express();
+
+// Routes Config
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+
+// Two routes one for post and one for get
+app.use("/", require("./routes/redirect"));
+app.use("/api/url", require("./routes/url"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`server started, listening PORT ${PORT}`));
